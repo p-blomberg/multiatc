47dfadee7e72b977a56bd138068cb465e21f0ae0
Petter Blomberg <github@petterblomberg.se>
2013-07-03 17:59:33 +0200

    More crap from html5 boilerplate

--- views/html_head.php
+++ views/html_head.php
@@ -1,7 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
-    <meta charset="utf-8">
-    <title><?=$title?></title>
+	<meta charset="utf-8">
+	<title><?=$title?></title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="/css/normalize.min.css">
+	<link rel="stylesheet" href="/css/main.css">
+	<!--[if lt IE 9]>
+		<script src="/js/vendor/html5shiv.js"></script>
+	<![endif]-->
 </head> 
 <body>
