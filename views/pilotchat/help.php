a34de2af501925440a6cd40fd81d75987e143e60
Petter Blomberg <git@petterblomberg.se>
2013-10-19 01:44:44 +0200

    Fixing missing help

--- views/pilotchat/help.php
+++ views/pilotchat/help.php
@@ -4,7 +4,9 @@ flight_number command info1 info2 info3<br>
 Commands:<br>
 Speed: s [knots]<br>
 Altitude: a [thousands of feet]<br>
+Heading: h [degrees]<br>
 <br>
 Examples:<br>
 AA123 s 260 - American Airlines flight 260, make your speed 260 knots<br>
 SAS468 a 5 - Scandinavian Airlines flight 468, maintain 5000 feet<br>
+SAS468 h 270 - Scandinavian Airlines flight 468, turn to heading 270 (west)<br>
